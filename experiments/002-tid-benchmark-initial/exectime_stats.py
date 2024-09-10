11f27963ea12384ca650d42cbc76b076cf3f09c5
Claudio Cicconetti <c.cicconetti@iit.cnr.it>
2024-09-10 16:27:05 +0200

    Experiment 002: update post-processing scripts

--- experiments/002-tid-benchmark-initial/exectime_stats.py
+++ experiments/002-tid-benchmark-initial/exectime_stats.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import pandas as pd
+import numpy as np
 import os
 
 filename = os.environ.get("DATASET", "")
@@ -11,19 +12,21 @@ warmup = float(os.environ.get("WARMUP", "0.1"))
 assert 0 <= warmup <= 1
 
 df = pd.read_csv(filename)
-df = df[df["entity"] == "w"]
 min_ts, max_ts = df["timestamp"].min(), df["timestamp"].max()
 warmup_ts = min_ts + (max_ts - min_ts) * warmup
 df = df[df["timestamp"] >= warmup_ts]
+df["value"] = df["value"].apply(lambda x: x * 1000)
 
-grouped = df.groupby("name")["value"]
-grouped_ts = df.groupby("name")["timestamp"]
+grouped = df.groupby("identifier")["value"]
+grouped_ts = df.groupby("identifier")["timestamp"]
+tpts = grouped_ts.count() / (grouped_ts.max() - grouped_ts.min())
+tpts.replace(np.inf, np.nan, inplace=True)
 print(
-    "avg {:.2f}, mean-jitter {:.2f}, mean-q95 {:.2f}, q95-q95 {:.2f}, tpt {:.2f}".format(
+    "avg {:.2f}, mean-jitter {:.2f}, mean-q95 {:.2f}, q95-q95 {:.2f}, avg tpt {:.2f}".format(
         df["value"].mean(),
         (grouped.quantile(0.95) - grouped.quantile(0.05)).mean(),
         grouped.quantile(0.95).mean(),
         grouped.quantile(0.95).quantile(0.95),
-        (grouped_ts.count() / (grouped_ts.max() - grouped_ts.min())).mean(),
+        tpts.dropna().mean(),
     )
 )
--- experiments/002-tid-benchmark-initial/post.sh
+++ experiments/002-tid-benchmark-initial/post.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-names="small big"
+names="4small 4small-1big"
 
 for name in $names ; do
     echo "** $name"
     echo -n "workflow latency: "
     DATASET=dataset-$name/application_metrics.csv python wf_stats.py
+    echo -n "function execution time: "
+    DATASET=dataset-$name/performance_samples.csv python exectime_stats.py
     DATASET=dataset-$name/health_status.csv python health_stats.py
 done
