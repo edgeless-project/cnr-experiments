7f9ace9024087f5e5d5d43fb5ce960158c896e21
Claudio Cicconetti <c.cicconetti@iit.cnr.it>
2024-09-11 15:16:00 +0200

    Fix download-artifacts.sh script

--- scripts/download-artifacts.sh
+++ scripts/download-artifacts.sh
@@ -10,10 +10,4 @@ if [ "$PRINT_ONLY" != "" ] ; then
   exit 0
 fi
 
-if [ -d data ] ; then
-  echo "data directory already present: cowardly refusing to proceed"
-  exit 1
-fi
-
-mkdir data 2> /dev/null
 wget http://turig.iit.cnr.it/~claudio/public/edgeless/cnr-experiments/$experiment-$sub.tgz -O- | tar zx
