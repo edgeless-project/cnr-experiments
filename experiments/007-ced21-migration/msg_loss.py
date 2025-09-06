8d6cea58a2a7bc0080a88f2be2596d8defc5312a
Claudio Cicconetti <c.cicconetti@iit.cnr.it>
2025-09-06 17:05:23 +0000

    Experiment 007: update scripts

--- experiments/007-ced21-migration/msg_loss.py
+++ experiments/007-ced21-migration/msg_loss.py
@@ -12,11 +12,12 @@ SHOW = bool(os.environ.get("SHOW", ""))
 dfs = []
 experiments = next(os.walk(DATA_DIR))[1]
 losts = []
+tots = []
 for experiment in experiments:
 
     lost = 0
+    tot = 0
     for filename in next(os.walk(f"{DATA_DIR}/{experiment}/"))[2]:
-        print(filename)
         if "output" not in filename:
             continue
         with open(f"{DATA_DIR}/{experiment}/{filename}", "r") as infile:
@@ -28,11 +29,12 @@ for experiment in experiments:
                     if last_value == None:
                         last_value = value
                         continue
-                    print(f"{experiment} {line_cnt} {value} {last_value}")
-                    if value > last_value:
-                        lost += (value - last_value) / 2 - 1
+                    if value <= last_value:
+                        raise RuntimeError
+                    lost += (value - last_value) / 2 - 1
                     last_value = value
                     line_cnt += 1
+                    tot += 1
                 except:
                     print(
                         f"error in line {line_cnt} of file '{DATA_DIR}/{experiment}/{filename}': {line}"
@@ -40,26 +42,34 @@ for experiment in experiments:
                     raise
 
     losts.append(lost)
+    tots.append(tot)
 
 print(experiments)
 print(losts)
-assert False
+print(tots)
+loss_rates = [x / y for x, y in zip(losts, tots)]
+print(loss_rates)
 
-df = pd.concat(
-    [d.assign(experiment=name) for d, name in zip(dfs, experiments)],
-    ignore_index=True,
-)
+d = {
+    "experiment": experiments,
+    "loss": losts,
+    "messages": tots,
+    "loss_rate": loss_rates,
+}
+
+df = pd.DataFrame(data=d)
+
+df["messages"] = df["messages"].apply(lambda x: x / 100)
 
 metrics = [
-    ("proc_cpu_usage", "Process CPU usage"),
-    ("tot_throughput", "Network traffic per node (Mb/s)"),
-    ("active_power", "Active power (mW)"),
+    ("messages", "Total throughput (messages/s)"),
+    ("loss_rate", "Message loss rate"),
 ]
 
 basename = os.path.basename(os.getcwd())
 for y, ylabel in metrics:
     fig, ax = plt.subplots()
-    sns.boxplot(df, x="experiment", y=y, ax=ax)
+    sns.barplot(df, x="experiment", y=y, ax=ax)
     ax.set_ylabel(ylabel)
     show_or_save("{}-{}".format(basename, y))
 
