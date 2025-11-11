5dc0b65d270145de21e1ed702e8fe2d1f13fc0ac
Claudio Cicconetti <c.cicconetti@iit.cnr.it>
2025-11-11 16:37:33 +0100

    obj_track: add object path tracking

--- rust_functions/obj_track/src/lib.rs
+++ rust_functions/obj_track/src/lib.rs
@@ -27,7 +27,12 @@ struct Input {
     image_b64: String,
 }
 
-struct State {}
+type Objects = std::collections::VecDeque<std::collections::HashMap<String, (u32, u32)>>;
+
+#[derive(Default)]
+struct State {
+    objects: Objects,
+}
 
 fn draw_bbox(
     img: &mut image::DynamicImage,
@@ -61,25 +66,120 @@ fn draw_bbox(
     }
 }
 
+fn draw_paths(img: &mut image::DynamicImage, objects: &Objects, line_width: u32) {
+    let mut img = img;
+    let mut all_labels = std::collections::HashSet::new();
+    for object in objects {
+        for label in object.keys() {
+            all_labels.insert(label.clone());
+        }
+    }
+
+    for label in all_labels {
+        let mut last_midpoint: Option<(u32, u32)> = None;
+        let color = label_to_color(&label);
+        for object in objects {
+            if let Some(midpoint) = object.get(&label) {
+                if let Some(last_midpoint) = last_midpoint {
+                    for offset in 0..line_width as i32 {
+                        let real_offset = line_width as i32 / 2 - offset;
+                        draw_line(
+                            &mut img,
+                            midpoint.0 as i32,
+                            midpoint.1 as i32 + real_offset,
+                            last_midpoint.0 as i32,
+                            last_midpoint.1 as i32 + real_offset,
+                            color,
+                        );
+                    }
+                }
+
+                last_midpoint = Some(midpoint.clone());
+            }
+        }
+    }
+}
+
+// Bresenham’s Line Algorithm
+fn draw_line(
+    img: &mut image::DynamicImage,
+    mut x0: i32,
+    mut y0: i32,
+    x1: i32,
+    y1: i32,
+    color: image::Rgba<u8>,
+) {
+    let img_width = img.width() as i32;
+    let img_height = img.height() as i32;
+    let dx = (x1 - x0).abs();
+    let sx = if x0 < x1 { 1 } else { -1 };
+    let dy = -(y1 - y0).abs();
+    let sy = if y0 < y1 { 1 } else { -1 };
+    let mut err = dx + dy;
+
+    loop {
+        if x0 >= 0 && x0 < img_width && y0 >= 0 && y0 < img_height {
+            img.put_pixel(x0 as u32, y0 as u32, color);
+        }
+        if x0 == x1 && y0 == y1 {
+            break;
+        }
+        let e2 = 2 * err;
+        if e2 >= dy {
+            err += dy;
+            x0 += sx;
+        }
+        if e2 <= dx {
+            err += dx;
+            y0 += sy;
+        }
+    }
+}
+
+fn label_to_color(label: &str) -> image::Rgba<u8> {
+    let mut state = std::hash::DefaultHasher::new();
+    label.hash(&mut state);
+    let hash = state.finish();
+    let hash_bytes = hash.to_be_bytes();
+    image::Rgba([hash_bytes[0], hash_bytes[1], hash_bytes[2], 0])
+}
+
 fn process(input: &Input, conf: &Conf) -> anyhow::Result<Vec<u8>> {
     let vec: Vec<u8> = base64::engine::general_purpose::STANDARD.decode(&input.image_b64)?;
     let mut img = image::load_from_memory(vec.as_slice())?;
 
+    let mut new_objects = std::collections::HashMap::new();
+    let mut duplicates = std::collections::HashSet::new();
     for i in 0..input.boxes.len() {
         if i < input.labels.len() {
             let bbox = input.boxes[i];
-            let mut state = std::hash::DefaultHasher::new();
-            input.labels[i].hash(&mut state);
-            let hash = state.finish();
-            let hash_bytes = hash.to_be_bytes();
-            draw_bbox(
-                &mut img,
-                bbox,
-                image::Rgba([hash_bytes[0], hash_bytes[1], hash_bytes[2], 0]),
-                conf.width,
-            );
+            let label = input.labels[i].clone();
+            draw_bbox(&mut img, bbox, label_to_color(&label), conf.width);
+
+            if duplicates.contains(&label) {
+                continue;
+            }
+
+            let midpoint_x = (bbox[0] + bbox[2]) as u32 / 2;
+            let midpoint_y = (bbox[1] + bbox[3]) as u32 / 2;
+            if new_objects
+                .insert(label.clone(), (midpoint_x, midpoint_y))
+                .is_some()
+            {
+                new_objects.remove(&label);
+                duplicates.insert(label);
+            }
         }
     }
+
+    let mut state = STATE.get().unwrap().lock().unwrap();
+
+    state.objects.push_back(new_objects);
+    if state.objects.len() >= conf.window as usize {
+        state.objects.pop_front();
+    }
+
+    draw_paths(&mut img, &state.objects, conf.width);
 
     let mut bytes: Vec<u8> = Vec::new();
     img.write_to(
@@ -114,6 +214,7 @@ static STATE: std::sync::OnceLock<std::sync::Mutex<State>> = std::sync::OnceLock
 // Outputs:
 // - out: base64-encoded resized image, in JPEG
 // - err: human-readable error if the input is not valid
+// - next: send an empty message when done
 impl EdgeFunction for ObjTrack {
     fn handle_cast(_src: InstanceId, msg: &[u8]) {
         let conf = CONF.get().unwrap();
@@ -140,6 +241,8 @@ impl EdgeFunction for ObjTrack {
                     .as_bytes(),
             ),
         }
+
+        cast("next", &[]);
     }
 
     fn handle_call(_src: InstanceId, _encoded_message: &[u8]) -> CallRet {
@@ -173,6 +276,7 @@ impl EdgeFunction for ObjTrack {
             window,
             width,
         });
+        let _ = STATE.set(std::sync::Mutex::new(State::default()));
     }
 
     fn handle_stop() {
